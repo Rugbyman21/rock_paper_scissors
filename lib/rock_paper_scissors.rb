2ed71b28aa336aa568fa6a76398eecfce3a1d936
Guest User <student@epicodus.com>
2015-08-05 12:16:49 -0700

    create files,folders and initial specs

---
+++ lib/rock_paper_scissors.rb
@@ -0,0 +1,6 @@
+class String
+  define_method(:rock_paper_scissors) do |player2|
+    player1 = self
+    
+  end
+end
---
+++ spec/rock_paper_scissors_integration_spec.rb
@@ -0,0 +1,14 @@
+require('capybara/rspec')
+require('./app')
+Capybara.app = Sinatra::Application
+set(:show_exceptions, false)
+
+describe('the rock paper scissors path', {:type => :feature}) do
+  it('processes the entry of rock as object and scissors as argument') do
+    visit('/')
+    fill_in("player1", :with => 'rock')
+    fill_in("player2", :with => 'scissors')
+    click_button('Lets Play!')
+    expect(page).to have_content("Player1 wins!")
+  end
+end
