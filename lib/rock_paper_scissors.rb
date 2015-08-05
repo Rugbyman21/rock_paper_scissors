adb7e5b011faf4a4e8bbe820e5c490ab7da449b0
Guest User <student@epicodus.com>
2015-08-05 15:24:00 -0700

    implement a check for correct input and a link to the starting-page

--- lib/rock_paper_scissors.rb
+++ lib/rock_paper_scissors.rb
@@ -1,6 +1,13 @@
 class String
   define_method(:rock_paper_scissors) do |player2|
     player1 = self
+    valid_input=["rock","paper","scissors"]
+
+    if valid_input.include?(player1 && player2)
+      else
+        return "Please only enter rock, paper or scissors"
+    end
+
     if (player1 == player2)
       return "You got a tie!"
     elsif ((player1=="rock") && (player2=="scissors"))||
--- spec/rock_paper_scissors_integration_spec.rb
+++ spec/rock_paper_scissors_integration_spec.rb
@@ -12,3 +12,14 @@ describe('the rock paper scissors path', {:type => :feature}) do
     expect(page).to have_content("Player1 Wins!")
   end
 end
+
+
+describe('the rock paper scissors path', {:type => :feature}) do
+  it('processes the entry of invalid entries as both argument and object') do
+    visit('/')
+    fill_in("player1", :with => 'rooooock')
+    fill_in("player2", :with => 'scisssssssors')
+    click_button('Lets Play!')
+    expect(page).to have_content("Please only enter rock")
+  end
+end
