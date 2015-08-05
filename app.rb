70b7ebeefc6d0a32dfad2ef4ce8d4d6b326452e5
Guest User <student@epicodus.com>
2015-08-05 15:55:29 -0700

    add a single-player-input version which branches out from index

--- app.rb
+++ app.rb
@@ -8,12 +8,24 @@ get('/') do
   erb(:index)
 end
 
+get('/singleplayer') do
+  erb(:singleplayer)
+end
 
   get('/score') do
     @player1 = params.fetch("player1")
     @player2 = params.fetch("player2")
     #binding.pry
     @score = @player1.rock_paper_scissors(@player2)
+
+    erb(:score)
+  end
+
+
+  get('/singlescore') do
+    @player1 = params.fetch("player1")
+    #@player_comp = params.fetch("player_comp")
+    @score = @player1.rock_paper_scissors(@player_comp)
 
     erb(:score)
   end
--- lib/rock_paper_scissors.rb
+++ lib/rock_paper_scissors.rb
@@ -3,16 +3,26 @@ class String
     player1 = self
     valid_input=["rock","paper","scissors"]
 
-    if valid_input.include?(player1 && player2)
+    if valid_input.include?(player1 && player2 || player1)
       else
         return "Please only enter rock, paper or scissors"
     end
 
-    if (player1 == player2)
+    player_comp=""
+    random_nr = 1+rand(3)
+    if random_nr == 1
+      player_comp="rocks"
+    elsif random_nr == 2
+      player_comp="paper"
+    else
+      player_comp="scissors"
+    end
+
+    if (player1 == player2 || player1 == player_comp)
       return "You got a tie!"
-    elsif ((player1=="rock") && (player2=="scissors"))||
-      ((player1=="scissors") && (player2=="paper"))||
-      ((player1=="paper") && (player2=="rock"))
+    elsif ((player1=="rock") && (player2=="scissors" || player_comp=="scissors"))||
+      ((player1=="scissors") && (player2=="paper" || player_comp == "paper"))||
+      ((player1=="paper") && (player2=="rock" || player_comp == "rock"))
       return "Player1 Wins!"
     else
       return "Player2 Wins!"
